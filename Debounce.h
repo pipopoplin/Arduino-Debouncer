ed6bf0c8a84d55dbc4ecd1bc495d9650c5922fdc
pipopoplin <dauerblender@gmx.net>
2015-10-07 12:14:34 +0200

    Create Debounce.h

---
+++ Debounce.h
@@ -0,0 +1,38 @@
+/*
+  Debounce.h - Library for debouncing Pin
+  Created by Simon Aeschbacher, September 28, 2015
+  Released into the public domain.
+*/
+
+#if ARDUINO >= 100
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+class Debounce
+{
+  public:
+    Debounce(int pin, boolean pullup, boolean hur);			// pin(Pin on Arduino, en_pullup (Enables pullupresistor), hur (hold until release))
+	boolean dbRead(void);									// returns if switch is active
+	
+	void    set_pin(int pin);
+	int     get_pin(void);
+	void    set_pullup(boolean pullup);
+	boolean get_pullup(void);
+	void    set_hur(boolean hur);
+	boolean get_hur(void);
+	void    set_cycles(int cycles);
+	int     get_cycles(void);
+	void    set_dtus(int dtus);
+	int     get_dtus(void);
+	
+  private:
+    boolean _db(void);
+
+    int  	_pin;
+	boolean _pullup;
+	boolean _hur;
+	int     _cycles = 20;
+	int     _dtus = 100;
+};
